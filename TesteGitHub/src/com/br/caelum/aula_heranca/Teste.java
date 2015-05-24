1cfd860e2a2fd11ae57073eacc12f360e79a48a3
diegomastsilva <diegomastsilva@gmail.com>
2015-05-23 23:40:28 -0300

    Fiz uma alteração na classe Teste, e estou add, uma nova classe Diretor

--- TesteGitHub/src/com/br/caelum/aula_heranca/Teste.java
+++ TesteGitHub/src/com/br/caelum/aula_heranca/Teste.java
@@ -7,14 +7,8 @@ public class Teste {
 		
 		Funcionarios objFunc1 = new Funcionarios("Diego Silva","02234-777",3000.000);	
 		
-		System.out.println(objFunc1.toString());
 		
-/*		Gerente objGerente1 = new Gerente(123, 10);
-		objGerente1.setNome("Diego");
-		objGerente1.setCpf("3322145-88");
-		objGerente1.setSalario(5000.000);*/
-		
-		//System.out.println(objGerente1.toString());
+		System.out.println();
 		
 				
 	}
